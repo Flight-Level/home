0495c579e7513d0be6f887dedffb6f98d059a3af
Daniel <blt950@users.noreply.github.com>
2025-03-13 17:53:14 +0100

    feat: new website (#1)

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # Flight Level Homepage
-Information site for Norway Live.
+Created with Astro JS.
+
+## Installation
+
+To install and run this project locally, follow these steps:
+
+1. Clone the repository:
+
+   ```bash
+   git clone (...)
+   ```
+2. Navigate to the project directory:
+
+   ```bash
+   cd home
+   ```
+3. Install dependencies:
+
+   ```bash
+   npm i
+   ```
+
+## Usage
+
+Run local dev
+   ```bash
+   npm run dev
+   ```
+
+Run build
+   ```bash
+   npm run build
+   ```
+The compiled website will be available in the dist directory.
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,138 @@
+"use client"
+
+import * as React from "react"
+import { ExternalLinkIcon } from './icons/ExternalLinkIcon';
+
+import { cn } from "../lib/utils"
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+} from "./ui/Navigation-menu"
+import { ComputerIcon } from "./icons/ComputerIcon";
+import { TicketIcon } from "./icons/TicketIcon";
+import { PresentationIcon } from "./icons/PresentationIcon";
+
+type NavigationItem = {
+    icon?: any; title: string; href: string; description: string, external?: boolean
+}
+type NavigationItems = Array<NavigationItem>;
+
+const events: NavigationItems = [
+    {
+        icon: ComputerIcon,
+        title: "LAN-party",
+        href: "/norway-live-2025/lan",
+        description: "Bring your rig for the whole weekend",
+        external: false,
+    },
+    {
+        icon: PresentationIcon,
+        title: "Mini-Expo",
+        href: "/norway-live-2025/expo",
+        description: "Saturday presentations and stands",
+        external: false,
+    },
+    {
+        icon: TicketIcon,
+        title: "LAN-tickets",
+        href: "https://forms.gle/TYVSk4v57VhtjfoB9",
+        description: "Purchase a seat for the LAN-party",
+        external: true,
+    },
+]
+
+export default function Navigation() {
+    return (
+        <NavigationMenu >
+            <NavigationMenuList className="flex flex-col md:flex-row w-full items-center">
+                <NavigationMenuItem>
+                    <NavigationMenuTrigger>Norway Live 2025</NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                        <ul className="grid w-[100%] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+                            {events.map((component) => (
+                                <ListItem
+                                    key={component.title}
+                                    icon={component.icon}
+                                    title={component.title}
+                                    href={component.href}
+                                    external={component.external}
+                                >
+                                    {component.description}
+                                </ListItem>
+                            ))}
+                        </ul>
+                    </NavigationMenuContent>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <NavigationMenuLink asChild>
+                        <a
+                            href="/sponsor"
+                            className="block px-4 py-2 text-base font-medium transition-colors rounded-md hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+                        >
+                            Sponsoring
+                        </a>
+                    </NavigationMenuLink>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <NavigationMenuLink asChild>
+                        <a
+                            href="/about"
+                            className="block px-4 py-2 text-base font-medium transition-colors rounded-md hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+                        >
+                            About us
+                        </a>
+                    </NavigationMenuLink>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <NavigationMenuLink asChild>
+                        <a
+                            href="mailto:hei@flightlevel.no"
+                            className="block px-4 py-2 text-base font-medium transition-colors rounded-md hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+                        >
+                            Contact us
+                        </a>
+                    </NavigationMenuLink>
+                </NavigationMenuItem>
+            </NavigationMenuList>
+        </NavigationMenu>
+    )
+}
+
+interface NavigationListItemProps extends React.ComponentPropsWithoutRef<"a"> {
+    icon?: any,
+    title: string;
+    external?: boolean;
+}
+
+const ListItem = React.forwardRef<HTMLAnchorElement, NavigationListItemProps>(
+    ({ className, icon, title, children, external, ...props }, ref) => {
+        return (
+            <li>
+                <NavigationMenuLink asChild>
+                    <a
+                        ref={ref}
+                        target={external ? "_blank" : undefined}
+                        className={cn(
+                            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+                            className
+                        )}
+                        {...props}
+                    >
+                        {icon && icon({ width: "1.5rem", marginLeft: 0 })}
+                        <div className="text-sm font-medium leading-none">
+                            {title}
+                            {external == true && <ExternalLinkIcon width="0.75rem" marginLeft="0.3rem" />}
+                        </div>
+                        <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                            {children}
+                        </p>
+                    </a>
+                </NavigationMenuLink>
+            </li>
+        )
+    })
+ListItem.displayName = "ListItem"
